f9fe98d968b7b30a62eb1f08c2135878e13d918a
Zofia Sitek <zofia.a.sitek@gmail.com>
2023-12-06 23:40:51 +0100

    .

---
+++ traits.py
@@ -0,0 +1,16 @@
+traits = [
+    'Sensitive',
+    'Introvert',
+    'Family-oriented',
+    'Protective',
+    'Friendly',
+    'Extravert',
+    'Quick lerner',
+    'Intelligent',
+    'Communicative',
+    'Curious',
+    'Energetic',
+    'Emotional',
+    'Sport freak',
+    'Playfull'
+]
